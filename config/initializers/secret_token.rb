ebff908466743c6e5926c9ae44776bfcc9c9bb7f
sanfran216 <sanfran216@gmail.com>
2012-10-15 16:13:02 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hilario::Application.config.secret_token = '81d861f0cc642e8533640fff300e74ae60874c5ff05b9541c9c0ca250c97976816037d45de6d69379c0b754eb5887fa178d0876523f0f57ce48a700c426fb7e1'
